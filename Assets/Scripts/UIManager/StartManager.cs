aa1dbe5d51c8de5a91167a06f95b45bcf58e39b3
seonhjeo <hoya011196@gmail.com>
2023-01-25 13:51:35 +0900

    make new scene and a panel

---
+++ Assets/Scripts/UIManager/StartManager.cs
@@ -0,0 +1,44 @@
+
+using UnityEngine;
+
+
+namespace UIManager
+{
+    public partial class StartManager // Properties and Methods that other classes can use
+    {
+        public bool Status { get; private set; } = true;
+        
+        public void SetStatus(bool status) => _SetStatus(status);
+
+        public void QuitGame() => _QuitGame();
+    }
+
+    public partial class StartManager // Properties and Methods that only this class use
+    {
+        
+    }
+    
+    public partial class StartManager : MonoBehaviour
+    {
+
+    }
+
+    public partial class StartManager : IUIManager
+    {
+        private void _SetStatus(bool status)
+        {
+            Status = status;
+            gameObject.SetActive(status);
+        }
+
+        
+        
+        private void _QuitGame()
+        {
+            // TODO : change at build
+            UnityEditor.EditorApplication.isPlaying = false;
+            // Application.Quit();
+        }
+    }
+}
+

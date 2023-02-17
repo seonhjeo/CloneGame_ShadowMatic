5bd848016c9a7f8b2a2ff55edb514c0c21399234
seonhjeo <hoya011196@gmail.com>
2023-02-17 13:06:01 +0900

    make DataManager

--- Assets/Scripts/ScriptableObj/LoadLevelSo.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Level Load Data", menuName = "Scriptable Object/Level Load Data")]
-public class LoadLevelSo : ScriptableObject
-{
-    public int levelToLoad = -1;
-}
